a9a27ecb90ffe2885e9e97284406fb4341a9198f
Mamontoboi <mamontoboi@gmail.com>
2023-04-09 15:41:20 +0200

    Added Selenium page functioning and tests

--- main.py
+++ main.py
@@ -5,21 +5,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from abc import ABC, abstractmethod
 
 
-class BasePage:
+class Driver:
     def __init__(self):
         self.PATH = "C:\\Program Files (x86)\\chromedriver.exe"
         self.service = Service(self.PATH)
-        self.driver = webdriver.Chrome(service=self.service)
-        self.driver.implicitly_wait(3)
+        self.test_driver = webdriver.Chrome(service=self.service)
+        self.test_driver.implicitly_wait(3)
+
+    @property
+    def driver(self):
+        return self.test_driver
 
 
-class MainWikiPage(BasePage):
+class AbstractPage(ABC):
+    def __init__(self, page_driver):
+        self.driver = page_driver
 
-    def __init__(self):
-        super().__init__()
-        self.driver.get("https://www.wikipedia.org/")
+    @abstractmethod
+    def is_title_matches(self):
+        pass
+
+
+class MainWikiPage(AbstractPage):
 
     def is_title_matches(self):
         return "Wikipedia" in self.driver.title
@@ -28,17 +38,63 @@ class MainWikiPage(BasePage):
         return self.driver.find_element(By.ID, "searchInput")
 
     def search_for_selenium(self):
-        searchbar = self.locate_searchbar()
-        searchbar.clear()
-        searchbar.send_keys("Selenium (software)")
-        searchbar.send_keys(Keys.RETURN)
+        page_searchbar = self.locate_searchbar()
+        page_searchbar.clear()
+        page_searchbar.send_keys("Selenium (software)")
+        page_searchbar.send_keys(Keys.RETURN)
         return "Selenium" in self.driver.title
 
 
+class SeleniumWikiPage(AbstractPage):
+
+    def is_title_matches(self):
+        return "Selenium" in self.driver.title
+
+    def wait_for_it(self, class_name):
+        WebDriverWait(driver=self.driver, timeout=10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, class_name))
+        )
+        return True
+
+    def find_chapters(self):
+        if self.wait_for_it("image"):
+            elements = self.driver.find_elements(By.TAG_NAME, "h3")
+            cleared_elements = set()
+            for element in elements:
+                element_text = element.text
+                if element_text.endswith('[edit]'):
+                    cleared_elements.add(element_text[:-6])
+                else:
+                    cleared_elements.add(element_text)
+
+            return cleared_elements
+
+
 if __name__ == '__main__':
-    page = MainWikiPage()
-    print(page.is_title_matches())
-    print(page.search_for_selenium())
+    PATH = "C:\\Program Files (x86)\\chromedriver.exe"
+    service = Service(PATH)
+    driver = webdriver.Chrome(service=service)
+    driver.implicitly_wait(3)
+
+    page = MainWikiPage(driver)
+    page.driver.get("https://www.wikipedia.org/")
+    searchbar = page.locate_searchbar()
+    searchbar.clear()
+    searchbar.send_keys("Selenium (software)")
+    searchbar.send_keys(Keys.RETURN)
+
+    try:
+        selenium = SeleniumWikiPage(driver)
+        selenium_page_chapters = selenium.find_chapters()
+
+        for item in selenium_page_chapters:
+            print(item)
+
+    except WebDriverWait as e:
+        print(e)
+        page.driver.quit()
+
+
 
 
 
--- test_pytest/test_main.py
+++ test_pytest/test_main.py
@@ -1,8 +1,8 @@
 import pytest
 from unittest.mock import Mock, MagicMock, PropertyMock
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webelement import WebElement
 
 import main
@@ -11,7 +11,13 @@ import main
 class TestMainWikiPage:
 
     def setup_method(self):
-        self.page = main.MainWikiPage()
+        service = Service("C:\\Program Files (x86)\\chromedriver.exe")
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        self.driver = webdriver.Chrome(service=service, options=option)
+        self.driver.implicitly_wait(3)
+        self.page = main.MainWikiPage(self.driver)
+        self.page.driver.get("https://www.wikipedia.org/")
 
     def teardown_method(self):
         self.page.driver.quit()
@@ -30,6 +36,41 @@ class TestMainWikiPage:
     def test_is_title_matches_fail(self):
         type(self.page.driver).title = PropertyMock(return_value="You will not pass!")
         assert self.page.is_title_matches() is False
+
+
+class TestSeleniumWikiPage:
+
+    def setup_method(self):
+        service = Service("C:\\Program Files (x86)\\chromedriver.exe")
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        self.driver = webdriver.Chrome(service=service, options=option)
+        self.driver.implicitly_wait(3)
+        self.page = main.SeleniumWikiPage(self.driver)
+        self.page.driver.get("https://en.wikipedia.org/wiki/Selenium_(software)")
+
+    def teardown_method(self):
+        self.page.driver.quit()
+
+    def test_is_title_matches_pass(self):
+        assert self.page.is_title_matches() is True
+
+    def test_wait_for_it_pass(self):
+        assert self.page.wait_for_it("image") is True
+
+    def test_wait_for_it_fail(self):
+        with pytest.raises(TimeoutException):
+            self.page.wait_for_it("pycon")
+
+    def test_find_chapters_pass(self):
+        expecter_chapters = {"Selenium IDE", "Selenium WebDriver", "Selenium Grid",
+                             "Selenium client API", "Selenium Remote Control"}
+        chapters = self.page.find_chapters()
+        assert chapters == expecter_chapters
+
+    def test_find_chapters_fail(self):
+        chapters = self.page.find_chapters()
+        assert (chapters == {None}) is False
 
 
 if __name__ == '__main__':
